13384c7e6bee106934c6cda8f275684a3d937916
gabrielteran11 <gabrielteran.2111@gmail.com>
2021-02-23 11:37:08 -0500

    Agregando el html de el portafolio

---
+++ scripts.js
@@ -0,0 +1,23 @@
+ScrollReveal().reveal('.header', {
+  delay: 500,
+
+});
+ScrollReveal().reveal('.h1', {
+  delay: 1000,
+
+});
+ScrollReveal().reveal('.fondoCode', {
+  delay: 1500,
+
+});
+
+ScrollReveal().reveal('.scroll', {
+  reset: true,
+  //delay: 1000,
+  interval: 1000
+});
+ScrollReveal().reveal('.footer', {
+  reset: true,
+  delay: 3500,
+  //interval: 600
+});
