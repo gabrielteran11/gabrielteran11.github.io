379d6b689286f9ccf5be4588cbcddc9433a3432a
gabrielteran11 <gabrielteran.2111@gmail.com>
2021-02-23 14:45:07 -0500

    pequeño cambio

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
 ScrollReveal().reveal('.header', {
-  delay: 500,
+    delay: 500,
 
 });
 ScrollReveal().reveal('.h1', {
-  delay: 1000,
+    delay: 1000,
 
 });
 ScrollReveal().reveal('.fondoCode', {
-  delay: 1500,
+    delay: 1500,
 
 });
 
 ScrollReveal().reveal('.scroll', {
-  reset: true,
-  //delay: 1000,
-  interval: 1000
+    reset: true,
+    //delay: 1000,
+    interval: 1000
 });
 ScrollReveal().reveal('.footer', {
-  reset: true,
-  delay: 3500,
-  //interval: 600
+    //reset: true,
+    delay: 200,
+    //interval: 600
 });
